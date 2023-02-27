aa318feffa555d61f602a2b49b776be1080cf440
Magali Cabrera <magymonse24@gmail.com>
2023-02-27 18:38:38 -0300

    Add calculator functions

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,12 +1,73 @@
-<script setup>
+<script>
+export default{
+  data(){
+    return{
+      calculatorValue: '',
+      calculatorElements: ['C','*','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+      operator: null,
+      previousCalculatorValue: '',
+    }
+  },
+  methods: {
+    action(n){
+      /* Append value */
+      if(!isNaN(n) || n === '.'){
+        this.calculatorValue += n + '';
+      }
+      /* Clear value */
+      if(n === 'C'){
+        this.calculatorValue = '';
+      }
+      /* Percentage */
+      if(n === '%'){
+        this.calculatorValue = this.calculatorValue / 100 + '';
+      }
+      /* Operators */
+      if(['/','*','-','+'].includes(n)){
+        this.operator = n;
+        this.previousCalculatorValue = this.calculatorValue;
+        this.calculatorValue = '';
+      }
+      /* Calculate result using the eval function */
+      if(n === '='){
+        this.calculatorValue = eval(
+          this.previousCalculatorValue + this.operator + this.calculatorValue
+        );
+        this.previousCalculatorValue = '';
+        this.operator = null;
+      }
+    }
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    Hello World
+  <div class="p-3" style="max-width: 400px; margin: 50px auto; backgroud: #234">
+    <div class="w-full rounded m-1 p-3 text-end lead font-weight-bold text-white bg-vue-dark">
+      {{ calculatorValue || 0 }}
+    </div>
+    <div class="row no-gutters">
+      <div class="col-3" v-for="n in calculatorElements" key="n">
+        <div class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class" 
+        :class= "{'bg-vue-green': ['C','*','/','-','+','%','='].includes(n)}"
+          @click="action(n)">
+          {{ n }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.bg-vue-dark{
+ background: #31475e;
+}
+.hover-class:hover{
+  cursor: pointer;
+  background: #305875;
+}
+.bg-vue-green {
+  background: #3fb984;
+}
 </style>
